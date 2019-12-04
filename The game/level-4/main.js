1c2a17ddab616cc284d48ced6fc904915c48da02
Noah Satz <satznoah@gmail.com>
2019-12-04 21:16:50 +0200

    I changed the dialogue, set levels 1-5 and made a new functional battle screen which needs more editing

---
+++ The game/level-4/main.js
@@ -0,0 +1,89 @@
+//COLLECT INGREDIENTS
+
+//<P> TAG
+const text = document.getElementById('text');
+const sceneImage = document.getElementById('scene-image');
+let sceneNmbr = 0;
+
+let i = 0;
+let txt = `The first day on the job! Time to develop some JavaScript!`;
+let speed = 30;
+
+//WHEN THE WINDOW LOADS START THE FIRST SCENE
+window.onload = function () {
+    nextScene();
+};
+
+function typeWriter() {
+    if (i < txt.length) {
+        text.innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+function nextScene() {
+
+    //PLEASE CLEAR ALL THE TEXT
+    text.innerHTML = '';
+
+    //STOP THE TYPE WRITER LOOP
+    stopAddingWords();
+
+
+    //JUMP TO THE NEXT DATA-TEXT ATTRIBUTE
+    if (!(text.getAttribute(`data-text-${sceneNmbr}`) === null)) {
+
+        nextText();
+        nextImage();
+
+        //IT HELPS US RESTART THE TYPING EFFECT
+        restartTypingEffect();
+
+        //SET THE NEXT SCENE
+        sceneNmbr = sceneNmbr + 1;
+    }
+
+    else {
+        //MOVE TO THE CODE EDITOR
+    }
+
+}
+
+function stopAddingWords() {
+    //STOP THE LOOP
+    txt = '';
+}
+
+function nextText() {
+    txt = text.getAttribute(`data-text-${sceneNmbr}`);
+
+    //REMOVE THE DATA TEXT
+    text.removeAttribute(`data-text-${sceneNmbr}`);
+}
+
+function nextImage() {
+    sceneImage.src = sceneImage.getAttribute(`data-image-src-${sceneNmbr}`);
+}
+
+
+function restartTypingEffect () {
+    i = 0;
+    typeWriter();
+}
+
+//WHEN YOU PRESS X...
+document.onkeydown = function (event) {
+//IF YOU CLICK ANY OTHER KEY ON THE KEYBOARD ASIDE FROM X
+    if (!(event.keyCode = 88)) {
+        return;
+    }
+
+    //IF YOU PRESS X AND ALL THE WORDS HAVE BEEN ADDED
+    if (!(i < txt.length)) {
+        nextScene();
+    } else {
+        text.innerText = txt;
+        stopAddingWords();
+    }
+};
