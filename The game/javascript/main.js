a3051457ae5515d62a2ccaab717394796b66916e
Tashinga <mtokotashinga@gmail.com>
2020-01-19 18:11:33 +0200

    98%git add . !

--- The game/javascript/main.js
+++ The game/javascript/main.js
@@ -2,7 +2,7 @@ const gameContainer = document.getElementById('game-container');
 
 let level = 0;
 
-let stages = ['level-1', 'level-1-battle-screen', 'level-2', 'level-2-battle-screen', 'level-3', 'level-3-battle-screen', 'level-4', 'level-4-battle-screen'];
+let stages = ['level-1', 'popup-1', 'level-1-battle-screen' , 'popup-2',  'level-2', 'popup-3','level-2-battle-screen', 'popup-4', 'level-3', 'popup-5', 'level-3-battle-screen', 'popup-6','level-4', 'popup-7', 'level-4-battle-screen','level-5', 'level-5.2', 'level-5.3','popup-8', 'level-5-battle-screen', 'level-5.4', 'end-screen' ];
 
 
 function nextStage() {
---
+++ The game/javascript/popup.js
@@ -0,0 +1,10 @@
+const popUp = document.getElementById('popup');
+
+window.onload = function () {
+    setTimeout(() => {
+        popUp.classList.add('open');
+        this.clearTimeout;
+    }, 1000);
+
+    
+}
